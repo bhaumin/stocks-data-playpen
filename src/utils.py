12ed033e98f983a61b84134490c31323f935c3ee
Bhaumin Shah <bhaumin@gmail.com>
2020-06-24 17:45:03 +0530

    exclude blank lines and white spaces

--- src/utils.py
+++ src/utils.py
@@ -8,16 +8,18 @@ def list_files(dir_path):
 
 def get_markets():
   with open('../data/markets.txt') as f:
-    return f.read().split('\n')
+    return [mkt.strip() for mkt in f.read().split('\n') if mkt.strip() != '']
 
 
 def get_stock_symbols(mkt):
   with open(f'../data/symbols/{mkt}.txt') as f:
-    return f.read().split('\n')
+    return [sym.strip() for sym in f.read().split('\n') if sym.strip() != '']
 
 
 def main():
-  pass
+  print(get_markets())
+  print(get_stock_symbols('us'))
+  print(get_stock_symbols('in'))
 
 
 if __name__ == "__main__":
