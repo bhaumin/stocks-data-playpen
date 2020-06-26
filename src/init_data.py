21ea8f7255fe9e59177a2cfcfbc4cb16dcb73f20
Bhaumin Shah <bhaumin@gmail.com>
2020-06-27 02:35:52 +0530

    init market and stock symbols data

--- src/init_data.py
+++ src/init_data.py
@@ -1,11 +1,12 @@
 from os.path import join
-import mdb
+from mdb import write_many_records, db_to_use, add_index_on_date
+from config import *
 import utils
 from pprint import pprint
 
 
 def migrate_csv_data_to_db():
-  for mkt in get_markets():
+  for mkt in utils.get_markets():
     dir_path = '../data/csv/' + mkt
     files = utils.list_files(dir_path)
 
@@ -18,22 +19,42 @@ def migrate_csv_data_to_db():
 
       with open(join(dir_path, filename)) as csv_file:
         documents = utils.parse_ohlcv_csv(csv_file)
-        resp = mdb.write_many_records(mkt, collection, documents)
-        added_index = mdb.add_index_on_date(mkt, collection)
+        resp = write_many_records(db_to_use(mkt), collection, documents)
+        added_index = add_index_on_date(db_to_use(mkt), collection)
         print(f'Inserted: {len(resp.inserted_ids)} records')
         print(f'Index created: {added_index}')
 
 
 def init_markets():
-  pass
+  records = []
+  for mkt in utils.get_markets():
+    records.append({
+      "name": mkt
+    })
+
+  print(records)
+  resp = write_many_records(db_name_meta, 'markets', records)
+  print(f'Inserted: {len(resp.inserted_ids)} records')
 
 
 def init_stock_symbols():
-  pass
+  for mkt in utils.get_markets():
+    records = []
+    for symbol in utils.get_stock_symbols(mkt):
+      records.append({
+        "name": symbol,
+        "market": mkt
+      })
+
+    print(records)
+    resp = write_many_records(db_name_meta, 'stocks', records)
+    print(f'Inserted: {len(resp.inserted_ids)} records')
 
 
 def main():
   # migrate_csv_data_to_db()
+  # init_markets()
+  # init_stock_symbols()
   pass
 
 
