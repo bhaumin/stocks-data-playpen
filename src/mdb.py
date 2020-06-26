23e047157e8c776d4b8b009b1ca90725585bfb89
Bhaumin Shah <bhaumin@gmail.com>
2020-06-27 02:34:41 +0530

    refactor mdb - pass db_name instead of mkt

--- src/mdb.py
+++ src/mdb.py
@@ -11,13 +11,13 @@ def db_to_use(mkt):
   return db_name_in if mkt == IN_MKT else db_name_us
 
 
-def get_collections(mkt):
-  db = client[db_to_use(mkt)]
+def get_collections(db_name):
+  db = client[db_name]
   return db.list_collection_names()
 
 
-def add_index_on_date(mkt, coll_name):
-  db = client[db_to_use(mkt)]
+def add_index_on_date(db_name, coll_name):
+  db = client[db_name]
   coll = db[coll_name]
 
   if has_index(mkt, coll_name, 'date_1'):
@@ -26,49 +26,49 @@ def add_index_on_date(mkt, coll_name):
   return coll.create_index([ ('date', ASCENDING) ], unique=True)
 
 
-def has_index(mkt, coll_name, index_name):
-  return index_name in get_indexes(mkt, coll_name)
+def has_index(db_name, coll_name, index_name):
+  return index_name in get_indexes(db_name, coll_name)
 
 
-def get_indexes(mkt, coll_name):
-  db = client[db_to_use(mkt)]
+def get_indexes(db_name, coll_name):
+  db = client[db_name]
   coll = db[coll_name]
   return list(coll.index_information())
 
 
-def get_latest_record(mkt, coll_name):
-  db = client[db_to_use(mkt)]
+def get_latest_record(db_name, coll_name):
+  db = client[db_name]
   coll = db[coll_name]
   result = coll.find().sort("date", DESCENDING).limit(1)
   records = list(result)
   return records[0] if len(records) > 0 else None
 
 
-def write_one_record(mkt, coll_name, doc):
-  db = client[db_to_use(mkt)]
+def write_one_record(db_name, coll_name, doc):
+  db = client[db_name]
   coll = db[coll_name]
   return coll.insert_one(doc)
 
 
-def write_many_records(mkt, coll_name, docs):
-  db = client[db_to_use(mkt)]
+def write_many_records(db_name, coll_name, docs):
+  db = client[db_name]
   coll = db[coll_name]
   return coll.insert_many(docs)
 
 
-def delete_one_record(mkt, coll_name, qry):
-  db = client[db_to_use(mkt)]
+def delete_one_record(db_name, coll_name, qry):
+  db = client[db_name]
   coll = db[coll_name]
   return coll.delete_one(qry)
 
 
 def main():
-  # result = get_latest_record('us', 'BA')
+  # result = get_latest_record(db_to_use('us'), 'BA')
   # print(result)
-  # result = get_collections_list('us')
-  # result = get_indexes('us', 'AAPL')
+  # result = get_collections(db_to_use('us'))
+  # result = get_indexes(db_to_use('us'), 'AAPL')
   # print(result)
-  # add_index_on_date('us', 'AAPL')
+  # add_index_on_date(db_to_use('us'), 'AAPL')
   pass
 
 
